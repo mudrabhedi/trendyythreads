38c5ef075b4ca964875c0212a44bd98896abc76f
Mudra Bhedi <127314892+mudrabhedi@users.noreply.github.com>
2026-05-04 01:27:27 +0530

    Update CheckOutPage.jsx

--- frontend/src/pages/CheckOutPage.jsx
+++ frontend/src/pages/CheckOutPage.jsx
@@ -112,7 +112,7 @@ export default function CheckoutPage() {
 });
 
       const options = {
-        key: "rzp_test_SkAzOkzr0VpjvT",
+        key: import.meta.env.VITE_RAZORPAY_KEY_ID,,
         amount: order.amount,
         currency: order.currency,
         name: "TrendyThreads",
